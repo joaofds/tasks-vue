2c9755980affa221843f3907f8e61390db9554ec
Joao <jo4o.fds@gmail.com>
2022-12-27 19:04:56 -0300

    finaliza modal de nova tarefa

---
+++ src/components/elements/Modal.vue
@@ -0,0 +1,156 @@
+<script>
+  export default {
+    name: 'Modal',
+    methods: {
+      close() {
+        this.$emit('close');
+      },
+    },
+  };
+</script>
+
+<template>
+    <transition name="modal-fade">
+        <div class="modal-backdrop">
+            <div class="modal">
+                <header class="modal-header">
+                    <slot name="header">
+                        <h1>Cadastrar Tarefa</h1>
+                    </slot>
+                    <button 
+                        @click="close" 
+                        type="button" 
+                        class="btn-close"
+                    >
+                    <font-awesome-icon icon="xmark" />
+                    </button>
+                </header>
+
+                <section class="modal-body">
+                    <slot name="body">
+                        <div class="form-display">
+                            <label for="title">Título</label>
+                            <input
+                                type="text" 
+                                name="title"
+                                id="title"
+                            >
+                        </div>
+
+                        <div class="form-display">
+                            <label for="description">Descrição</label>
+                            <textarea
+                                rows="4"
+                            >
+                            </textarea>
+                        </div>
+                    </slot>
+                </section>
+
+                <footer class="modal-footer">
+                    <div class="radio-itens">
+                        <slot name="footer">
+                            <label for="urgente">Urgente</label>
+                            <input type="radio" name="priority" id="urgente">
+
+                            <label for="importante">Importante</label>
+                            <input type="radio" name="priority" id="importante">
+                        </slot>
+                    </div>
+                    <div class="buttons">
+                        <button @click="close" type="submit">
+                            Salvar
+                        </button>
+                    </div>
+                </footer>
+            </div>
+        </div>
+    </transition>
+</template>
+
+<style lang="stylus" scoped>
+.modal-backdrop 
+    position: fixed
+    top: 0
+    bottom: 0
+    left: 0
+    right: 0
+    background-color: rgba(0, 0, 0, 0.3)
+    display: flex
+    justify-content: center
+    align-items: center
+
+.modal
+    width: 660px
+    height: 488px
+    background: #fff
+    box-shadow: 2px 2px 20px 1px
+    border-radius: 4px
+    overflow-x: auto
+    display: flex
+    flex-direction: column
+
+.modal-body,
+.modal-header,
+.modal-footer 
+    padding: 30px
+    display: flex
+
+.modal-body
+    margin-top: -2em
+    display: flex
+    flex-direction: column
+
+.modal-header h1
+    font-size: 26px
+    font-weight: bold
+
+.modal-footer
+    margin-top: -4.5em
+    justify-content: space-between
+
+.btn-close 
+    position: absolute
+    top: 0
+    right: 0
+    border: none
+    font-size: 20px
+    padding: 10px
+    cursor: pointer
+    color: #4E657B
+    font-weight: bold
+    background: transparent
+
+.modal-fade-enter,
+.modal-fade-leave-to 
+    opacity: 0
+
+.modal-fade-enter-active,
+.modal-fade-leave-active 
+    transition: opacity .5s ease
+
+input[type=text]
+    width: 600px
+    height: 50px
+
+input[type=radio]
+    width: 26px
+
+button[type=submit]
+  width: 118px
+  height: 47px
+
+textarea
+    width: 600px
+
+.buttons
+    margin-left: auto
+
+.radio-itens
+    display: flex
+    align-self: center
+
+.radio-itens label
+    margin-right: 0.5em
+    margin-left: 0.5em
+</style>
