9ee60ff9ab6342ff3a001eddb2fdbde98b6161a9
Joao <jo4o.fds@gmail.com>
2023-01-02 13:15:30 -0300

    add vuex paara refatoração

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import store from "./store/store";
 
 //icons
 import { library } from "@fortawesome/fontawesome-svg-core";
@@ -13,6 +14,7 @@ Vue.component("font-awesome-icon", FontAwesomeIcon);
 import "./assets/css/main.css";
 
 new Vue({
+  store,
   router,
   render: (h) => h(App),
 }).$mount("#app");
